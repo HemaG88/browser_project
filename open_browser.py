e803d3e26d4767fc30bb97cbb7c392248c77f3e8
Hemavathi G <7259ghema@github.com>
2026-04-28 13:23:42 +0530

    updated browser automation code

--- open_browser.py
+++ open_browser.py
@@ -1,8 +1,16 @@
 import webbrowser
 
-url = input("Enter website: ")
+print("1. Open Google")
+print("2. Open YouTube")
+print("3. Open GitHub")
 
-if not url.startswith("http"):
-    url = "https://" + url
+choice = input("Enter your choice (1-3): ")
 
-webbrowser.open(url)
+if choice == "1":
+    webbrowser.open("https://www.google.com")
+elif choice == "2":
+    webbrowser.open("https://www.youtube.com")
+elif choice == "3":
+    webbrowser.open("https://github.com")
+else:
+    print("Invalid choice")
